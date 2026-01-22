800056dcf9aad867f6b1dd561932a5ebf15365c0
Nikolai <degnikolaj@gmail.com>
2026-01-22 21:40:37 +0100

    add files

---
+++ cohort62el_fs_react_redux/src/lessons/WeatherProject/types.ts
@@ -0,0 +1,13 @@
+import { type ReactNode } from "react";
+
+export interface LayoutProps {
+  children: ReactNode;
+}
+
+export interface EmployeeData {
+  id: string;
+  name: string;
+  surname: string;
+  age: string;
+  job_position: string;
+}

e449729afa9d9b0167b5140743977c7d93cbe7dc
AllaBeloshapka <allabeloshapka4@gmail.com>
2026-01-23 20:20:49 +0100

    Заполнила всю папку HomePage

--- cohort62el_fs_react_redux/src/lessons/WeatherProject/components/HomePage/HomePage.tsx
+++ cohort62el_fs_react_redux/src/lessons/WeatherProject/components/HomePage/HomePage.tsx
@@ -1,7 +1,59 @@
 
+import { useFormik } from "formik"
+import * as Yup from "yup"
 
-export default function HomePage() {
+import Button from "components/Button/Button"
+import Input from "components/Input/Input"
+
+import { HOME_FORM_VALUES } from "./types"
+import { HomeFormContainer, InputsContainer } from "./styles"
+import { useAppDispatch } from "store/hooks"
+import { weatherSliceAction } from "store/"
+import { WeatherData } from "lessons/WeatherProject/types"
+import { v4 } from "uuid"
+
+const validationShema = Yup.object().shape({
+  [HOME_FORM_VALUES.CITY]: Yup.string()
+    .required("City field is required")
+    .min(2, "minimum 2 simbols")
+    .max(50, "maximum 50 simbols"),
+
+})
+
+function HomePage() {
+  const dispatch = useAppDispatch()
+
+  const formik = useFormik({
+    initialValues: {
+      [HOME_FORM_VALUES.CITY]: "",
+    },
+    validationSchema: validationShema,
+    validateOnChange: false,
+    // Это функция, которая срабатывает при отправке формы
+    onSubmit: values => {
+      const newCity: WeatherData = { id: v4(), ...values }
+
+      dispatch(weatherSliceAction.personCard(newCity)) // передаем данные введенные пользователем с values
+      console.log("formik")
+      console.log(values)
+    },
+  })
   return (
-    <div>HomePage</div>
+    <HomeFormContainer onSubmit={formik.handleSubmit}>
+      <InputsContainer>
+        <Input
+          id="name-id"
+          name={HOME_FORM_VALUES.CITY}
+          placeholder="Enter your city"
+          label="City*"
+          value={formik.values[HOME_FORM_VALUES.CITY]}
+          onChange={formik.handleChange}
+          error={formik.errors[HOME_FORM_VALUES.CITY]}
+        />
+
+      </InputsContainer>
+      <Button name="Cearch" type="submit" />
+    </HomeFormContainer>
   )
 }
+export default HomePage

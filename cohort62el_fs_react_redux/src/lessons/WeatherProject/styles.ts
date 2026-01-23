9cba34c5eca24189d08837ac361fb26dfff256b5
Nikolai <degnikolaj@gmail.com>
2026-01-23 05:27:34 +0100

    add update Layout styles

--- cohort62el_fs_react_redux/src/lessons/WeatherProject/styles.ts
+++ cohort62el_fs_react_redux/src/lessons/WeatherProject/styles.ts
@@ -1,57 +1,58 @@
-import styled from "@emotion/styled";
-import { NavLink, type NavLinkRenderProps } from "react-router-dom";
+import styled from "@emotion/styled"
+import { NavLink, type NavLinkRenderProps } from "react-router-dom"
 
 export const LayoutWrapper = styled.div`
   display: flex;
   flex-direction: column;
   flex: 1;
-`;
+  background-image: url("/background.jpg");
+  background-size: cover;
+  background-position: center;
+`
 
 export const Header = styled.header`
   display: flex;
   justify-content: space-between;
   width: 100%;
-  height: 120px;
+  height: 80px;
   border-bottom: 2px solid black;
   padding: 42px 84px;
-`;
+  background-color: #122d4d80;
+`
 
 export const Logo = styled.div`
-display: flex;
-align-items: center;
-justify-content: center;
+  display: flex;
+  align-items: center;
+  justify-content: center;
   height: 100%;
-  width: 80px;
+  width: 180px;
   cursor: pointer;
-  font-weight: 700;
-  font-size: 16px;
-`;
+  font-weight: 900;
+  font-size: 30px;
+  color: white;
+`
 
 export const NavigationContainer = styled.nav`
   display: flex;
   gap: 56px;
   height: 100%;
   align-items: center;
- `;
+`
 
 export const HeaderLink = styled(NavLink)`
-  font-size: 28px;
+  font-size: 20px;
   font-weight: 400;
   text-decoration: none;
-  color: black;
-`;
+  color: white;
+`
 
 export const Main = styled.main`
   display: flex;
   justify-content: center;
   flex: 1;
   padding: 40px;
-  background-image: url("/background.jpg");
-`;
-
-
+`
 
 export const getActiveStyles = ({ isActive }: NavLinkRenderProps) => ({
   fontWeight: isActive ? "bold" : "normal",
-  textDecoration: isActive ? "underline" : "none",
-});
+})
